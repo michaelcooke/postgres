881e62e693e6d433f2a23f33b2ad3f3c4129ac0f
Michael Cooke <michael.frank.cooke@gmail.com>
2023-06-08 13:39:07 -0400

    Add everything from supabase/postgres

---
+++ ansible/files/admin_api_scripts/pg_upgrade_complete.sh
@@ -0,0 +1,81 @@
+#! /usr/bin/env bash
+
+## This script is run on the newly launched instance which is to be promoted to
+## become the primary database instance once the upgrade successfully completes.
+## The following commands copy custom PG configs and enable previously disabled
+## extensions, containing regtypes referencing system OIDs.
+
+set -eEuo pipefail
+
+# shellcheck disable=SC1091
+source ./pg_upgrade_common.sh
+
+LOG_FILE="/tmp/pg-upgrade-complete.log"
+
+function cleanup {
+    UPGRADE_STATUS=${1:-"failed"}
+    EXIT_CODE=${?:-0}
+
+    echo "$UPGRADE_STATUS" > /tmp/pg-upgrade-status
+
+    ship_logs "$LOG_FILE" || true
+
+    exit "$EXIT_CODE"
+}
+
+function complete_pg_upgrade {
+    if [ -f /tmp/pg-upgrade-status ]; then
+        echo "Upgrade job already started. Bailing."
+        exit 0
+    fi
+
+    echo "running" > /tmp/pg-upgrade-status
+
+    echo "1. Mounting data disk"
+    retry 3 mount -a -v
+
+    # copying custom configurations
+    echo "2. Copying custom configurations"
+    retry 3 copy_configs
+
+    echo "3. Starting postgresql"
+    retry 3 service postgresql start
+
+    echo "4. Running generated SQL files"
+    retry 3 run_generated_sql
+
+    sleep 5
+
+    echo "5. Restarting postgresql"
+    retry 3 service postgresql restart
+
+    echo "6. Starting vacuum analyze"
+    retry 3 start_vacuum_analyze
+}
+
+function copy_configs {
+    cp -R /data/conf/* /etc/postgresql-custom/
+    chown -R postgres:postgres /var/lib/postgresql/data
+    chown -R postgres:postgres /data/pgdata
+}
+
+function run_generated_sql {
+    if [ -d /data/sql ]; then
+        for FILE in /data/sql/*.sql; do
+            if [ -f "$FILE" ]; then
+                run_sql -f "$FILE"
+            fi
+        done
+    fi
+}
+
+function start_vacuum_analyze {
+    echo "complete" > /tmp/pg-upgrade-status
+    su -c 'vacuumdb --all --analyze-in-stages' -s "$SHELL" postgres
+    echo "Upgrade job completed"
+}
+
+trap cleanup ERR
+
+
+complete_pg_upgrade >> $LOG_FILE 2>&1 &
---
+++ ebssurrogate/files/unit-tests/unit-test-01.sql
@@ -0,0 +1,33 @@
+BEGIN;
+CREATE EXTENSION IF NOT EXISTS pgtap;
+SELECT plan(8);
+
+-- Check installed extensions
+SELECT extensions_are(
+    ARRAY[
+    'plpgsql',
+    'pg_stat_statements',
+    'pgsodium',
+    'pgtap',
+    'pg_graphql',
+    'pgcrypto',
+    'pgjwt',
+    'uuid-ossp',
+    'supabase_vault'
+     ]
+);
+
+
+-- Check schemas exists
+SELECT has_schema('pg_toast');
+SELECT has_schema('pg_catalog');
+SELECT has_schema('information_schema');
+SELECT has_schema('public');
+
+-- Check that service_role can execute certain pgsodium functions
+SELECT function_privs_are('pgsodium', 'crypto_aead_det_decrypt', array['bytea', 'bytea', 'uuid', 'bytea'], 'service_role', array['EXECUTE']);
+SELECT function_privs_are('pgsodium', 'crypto_aead_det_encrypt', array['bytea', 'bytea', 'uuid', 'bytea'], 'service_role', array['EXECUTE']);
+SELECT function_privs_are('pgsodium', 'crypto_aead_det_keygen', array[]::text[], 'service_role', array['EXECUTE']);
+
+SELECT * from finish();
+ROLLBACK;
